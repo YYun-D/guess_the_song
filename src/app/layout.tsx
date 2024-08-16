6615a18a0b84d101f5a55f5db9b1b517e3cf5645
YYun-D <yeo1998@naver.com>
2024-08-17 00:42:27 +0900

    feat: 메인,song 페이지 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Black_Han_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const black_Han_Sans = Black_Han_Sans({
+  subsets: ['latin'],
+  weight: "400"
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={black_Han_Sans.className}>{children}</body>
     </html>
   );
 }
---
+++ src/app/song/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect, useRef } from 'react';
+import YouTubePlayer from 'youtube-player';
+
+export default function SongPage() {
+  const playerRef = useRef(null);
+
+  useEffect(() => {
+    if (playerRef.current) {
+      const player = YouTubePlayer(playerRef.current);
+      player.loadVideoById('G4BuSPt9iNQ');
+    }
+  }, []);
+    return (
+      <main>
+        <div ref={playerRef}></div>
+      </main>
+    )
+  }
